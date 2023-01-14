0ce0cb353efdac07e50ed8fa871a36f8549ddd2b
Allison Drouillard <amd93@calvin.edu>
2023-01-14 15:10:51 -0500

    project0

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
+#include <string>
+#include <iostream>
+#include <stdio.h>
+using namespace std;
 
+/* got the code idea from includehelp.com*/
 int main() {
+   int value = 37;
+
+   printf("Decimal value: %d\n", value);
+   printf("Octal value: %o\n", value);
+   printf("Hexadecimal value: %x\n", value);
 
     return 0;
 }
